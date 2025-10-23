88e4432501e25b34116c6a9ac046721dd0409476
0ymane <kilwatest@gmail.com>
2025-10-23 22:34:20 +0100

    3 button to meet requirements

--- Tp_Quiz/script.js
+++ Tp_Quiz/script.js
@@ -17,14 +17,7 @@ function checkAnswers() {
     const form = document.getElementById('quizForm');
     const totalQuestions = 10;
 
-    /* RESET */
-    for (let i = 1; i <= totalQuestions; i++) {
-        const feedback = document.getElementById(`feedback-q${i}`);
-        if (feedback) feedback.textContent = '';
-      }
-    document.getElementById('totalScore').textContent = '';
 
-    /* Answer Logic */
     for (let i = 1; i <= totalQuestions; i++) {
         const qName = `q${i}`;
         const selected = form.querySelector(`input[name="${qName}"]:checked`);
@@ -43,9 +36,44 @@ function checkAnswers() {
     }
   }
 
-  // Show final score
+
   const totalScore = document.getElementById('totalScore');
   totalScore.textContent = `Your score: ${score} / ${totalQuestions}`;
-
-  totalScore.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
+
+function resetQuiz() {
+    form.reset();
+}
+
+function showAnswers() {
+    const correctAnswers = {
+      q1: 'c',
+      q2: 'c',
+      q3: 'b',
+      q4: 'b',
+      q5: 'c',
+      q6: 'c',
+      q7: 'a',
+      q8: 'b',
+      q9: 'c',
+      q10: 'a'
+    };
+
+    const form = document.getElementById('quizForm');
+    const totalQuestions = 10;
+
+    for (let i = 1; i <= totalQuestions; i++) {
+      const qName = `q${i}`;
+      const radios = form.elements[qName]; // all radios for this question
+      const feedback = document.getElementById(`feedback-q${i}`);
+
+      for (let j = 0; j < radios.length; j++) {
+        if (radios[j].value === correctAnswers[qName]) {
+          radios[j].checked = true;
+          feedback.textContent = '✅ Why Are u cheating! Study More...';
+        }
+      }
+    }
+
+    document.getElementById('totalScore').textContent = `All answers are now shown :(`;
+  }
